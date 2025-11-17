eda34d925560bc64f276f661b3ff0585e08f3b25
Soufiane-KS <soufiane.kousta@uit.ac.ma>
2025-11-17 14:21:43 +0100

    first commit

---
+++ src/test/java/com/example/ensetadriastockmarkettest/EnsetAdriaStockMarketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ensetadriastockmarkettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsetAdriaStockMarketTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
